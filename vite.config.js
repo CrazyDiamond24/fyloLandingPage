6f80437adf75df97e7e137d71f7bf13543795294
rania <raniakittan@hotmail.com>
2023-05-31 18:42:29 +0300

    issue with github pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-import { VitePWA } from 'vite-plugin-pwa'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     }
   }
 })
