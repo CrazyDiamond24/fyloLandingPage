335bbced0207d9b603c81d21c082bdeda87789a9
rania <raniakittan@hotmail.com>
2023-05-31 23:13:02 +0300

    dunno

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://crazydiamond24.github.io/Fylo-landing-page/',
   plugins: [vue()],
-  resolve: {
-    // No alias property here
-  }
 })
