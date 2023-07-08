4dffcb3b836f7b19d94bb4bb278ab59a85628a41
rania <raniakittan@hotmail.com>
2023-07-08 13:38:13 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://crazydiamond24.github.io/Fylo-landing-page/',
+  base: '/fyloLandingPage/', 
   plugins: [vue()],
+  build: {
+    outDir: 'dist'
+  }
 })
